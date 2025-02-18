175ee8b89ced76fee5626f45b7ebcc0cb3f7e398
haonanwu123 <wuhaonan99@gmail.com>
2025-02-18 09:31:25 +0100

    2O02 finish

--- W001.1.1T06/obj/Debug/net9.0/W001.1.1T06.AssemblyInfo.cs
+++ W001.1.1T06/obj/Debug/net9.0/W001.1.1T06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("W001.1.1T06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8b9940fd2e7dbcc233a7b7ff937d4ce21598ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ebd158291ee468f10c824ff51123062830ed3a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("W001.1.1T06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("W001.1.1T06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ W01.1.2O02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace W01._1._2O02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double discount = 0.1;
+        int price = 55;
+        double discountPrice = price * (1 - discount);
+
+        string message = $"The discount price is {discountPrice}";
+        Console.WriteLine(message);
+    }
+}
