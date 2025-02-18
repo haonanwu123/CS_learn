b9844d3c8c9d8224fc6dbaf561ff8a5314a0352b
haonanwu123 <wuhaonan99@gmail.com>
2025-02-18 19:01:59 +0100

    for loop learn

--- W001.1.1T05/obj/Debug/net9.0/W001.1.1T05.AssemblyInfo.cs
+++ W001.1.1T05/obj/Debug/net9.0/W001.1.1T05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("W001.1.1T05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b00819ed8759fa7eaa2b8b52b892238d2f9501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2171dce6e1ad9bda3e451657b9244db8502d5091")]
 [assembly: System.Reflection.AssemblyProductAttribute("W001.1.1T05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("W001.1.1T05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ W01.2.1T03/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace W01._2._1T03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is the person's name?");
+        string name = Console.ReadLine()!;
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == 2)
+            {
+                Console.WriteLine($"Happy birthday dear {name}!");
+            }
+            else
+            {
+                Console.WriteLine("Happy birthday to you!");
+            }
+        }
+    }
+}
