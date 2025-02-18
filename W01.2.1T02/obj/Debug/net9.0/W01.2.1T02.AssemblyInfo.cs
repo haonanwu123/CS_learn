c5329705c168d40407431e7501183059621d625a
haonanwu123 <wuhaonan99@gmail.com>
2025-02-18 19:47:48 +0100

    list learn

--- W01.2.1T02/obj/Debug/net9.0/W01.2.1T02.AssemblyInfo.cs
+++ W01.2.1T02/obj/Debug/net9.0/W01.2.1T02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("W01.2.1T02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9844d3c8c9d8224fc6dbaf561ff8a5314a0352b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d140420491948d97cd053242d5fda9f7571e9cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("W01.2.1T02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("W01.2.1T02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ W01.2.1T05/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace W01._2._1T05;
+
+static class Program
+{
+    static void Main()
+    {
+        List<string> pack = ["laptop", "lunch", "notebook", "pen"];
+
+        Console.WriteLine($"The first item in the pack is a {pack[0]}");
+        Console.WriteLine($"The last item in the pack is a {pack[pack.Count - 1]}");
+    }
+}
+
