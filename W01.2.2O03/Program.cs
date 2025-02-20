387b0c0bc385f2f11e678733ea7d550d64f79737
haonanwu123 <wuhaonan99@gmail.com>
2025-02-20 18:05:17 +0100

    fighter learn

---
+++ W01.2.2O03/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace W01._2._2O03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Fighter you = new();
+        Fighter enemy = new();
+
+        int turnsTaken = 0;
+
+        while (enemy.Health > 0)
+        {
+            enemy.Health -= you.Attack();
+            turnsTaken++;
+        }
+
+        Console.WriteLine($"The enemy's HP was reduced to {enemy.Health}");
+        Console.WriteLine($"It took you {turnsTaken} to defeat the enemy");
+    }
+}
