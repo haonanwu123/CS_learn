cfe3f06cfa79ce068ecc8185a93ebb8652f63578
haonanwu123 <wuhaonan99@gmail.com>
2025-02-28 13:12:06 +0100

    2O04 finish

---
+++ W01.2.2O04/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace W01._2._2O04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the start number: ");
+        int start = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Enter the end number: ");
+        int end = int.Parse(Console.ReadLine()!);
+
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+                Console.WriteLine("Buzz");
+            else
+                Console.WriteLine(i);
+        }
+    }
+}
