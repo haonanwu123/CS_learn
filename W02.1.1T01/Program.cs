22bf1dc152a434eda80215b4ab61c392e2ab296f
haonanwu123 <wuhaonan99@gmail.com>
2025-03-02 13:24:49 +0100

    done

---
+++ W02.1.1T01/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace W02._1._1T01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintFullName("John", "Doe");
+    }
+
+    public static void PrintFullName(string firstName, string lastName)
+    {
+        Console.WriteLine(firstName + " " + lastName);
+    }
+}
