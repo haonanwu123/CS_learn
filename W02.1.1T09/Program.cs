63d89ca5aeec8251cba9356b66ad04a7b3b39e5a
haonanwu123 <wuhaonan99@gmail.com>
2025-03-03 13:38:03 +0100

    done

---
+++ W02.1.1T09/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace W02._1._1T09;
+
+static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Give three names.");
+        Console.Write("The first name: ");
+        string name1 = Console.ReadLine()!;
+        Console.Write("The second name: ");
+        string name2 = Console.ReadLine()!;
+        Console.Write("The third name: ");
+        string name3 = Console.ReadLine()!;
+
+        Console.WriteLine($"{Longest(name1, name2, name3)} has the longest name");
+    }
+
+    static string Longest(string s1, string s2, string s3)
+    {
+        if (s1.Length > s2.Length && s1.Length > s3.Length) { return s1; }
+        if (s2.Length > s1.Length && s2.Length > s3.Length) { return s2; }
+        if (s3.Length > s1.Length && s3.Length > s2.Length) { return s3; }
+        return s3;
+    }
+}
+
