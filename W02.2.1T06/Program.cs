6b12858020c44c24881c8d7eeb414ed7726e04b2
haonanwu123 <wuhaonan99@gmail.com>
2025-03-03 20:18:48 +0100

    done

---
+++ W02.2.1T06/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace W02._2._1T06;
+
+static class Program
+{
+    static void Main()
+    {
+        /*Create a List of Pets here:
+         *- a Dog named Nugent
+         *- a Cat named Steve
+         *- a Goldfish named Brutus
+         */
+
+        List<Pet> pets = new List<Pet>
+         {
+            new Pet("Dog", "Nugent"),
+            new Pet("Cat", "Steve"),
+            new Pet("Goldfish", "Brutus")
+         };
+
+        foreach (Pet pet in pets)
+        {
+            Console.WriteLine($"I have a {pet.WhatAmI} named {pet.Name}");
+        }
+
+    }
+}
