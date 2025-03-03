56ded41576732aa4b756ab3df46557c2bc696df0
haonanwu123 <wuhaonan99@gmail.com>
2025-03-03 19:54:32 +0100

    done

---
+++ W02.2.1T02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace W02._2._1T02;
+
+static class Program
+{
+    static void Main()
+    {
+        Artist artist1 = new("Rihanna");
+        artist1.ComposeSong("Umbrella");
+        artist1.ComposeSong("Where Have You Been");
+
+        Artist artist2 = new("Michael Jackson");
+        artist2.ComposeSong("Billy Jean");
+        artist2.ComposeSong("Thriller");
+
+        foreach (Artist artist in new[] { artist1, artist2 })
+        {
+            foreach (Song song in artist.ComposedSongs)
+            {
+                Console.WriteLine(song.Info());
+            }
+        }
+    }
+}
+
