8109761b3f4a904bdac7874625367ad66469b706
haonanwu123 <wuhaonan99@gmail.com>
2025-03-03 20:13:59 +0100

    done

---
+++ W02.2.1T05/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace W02._2._1T05;
+class Program
+{
+    static void Main()
+    {
+        List<double> grades = [7, 5.5, 3.2, 10, 4.5];
+
+        int passCount = 0;
+        int totalCount = grades.Count;
+
+        foreach (double grade in grades)
+        {
+            if (grade >= 5.5)
+            {
+                passCount++;
+            }
+        }
+
+        Console.WriteLine($"{passCount} out of {totalCount} students passed");
+    }
+}
+
