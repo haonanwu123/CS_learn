81aae63baf90c8fdb8401198a96ea7d74f4c7dbf
haonanwu123 <wuhaonan99@gmail.com>
2025-03-04 09:47:51 +0100

    done

---
+++ W03.1.1T05/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace W03._1._1T05;
+
+static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your first name?");
+        string? firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string? lastName = Console.ReadLine();
+
+        string? fullName = GetFullName(firstName, lastName);
+        Console.WriteLine(fullName);
+    }
+
+    public static string? GetFullName(string? fName, string? lName) => $"{fName} {lName}";
+}
+
