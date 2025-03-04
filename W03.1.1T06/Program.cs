60a01f584a17bd4b5f3beeabd095da99dcd6a4e3
haonanwu123 <wuhaonan99@gmail.com>
2025-03-04 09:54:25 +0100

    done

---
+++ W03.1.1T06/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace W03._1._1T06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int spdOfLightInMperS = 299792458;
+        const double daysInYear = 365.242199;
+        const string planetName = "LP 890-9b";
+        const double distanceFromEarthInLightYear = 100;
+
+        double distanceInMeters = distanceFromEarthInLightYear * spdOfLightInMperS * daysInYear * 24 * 60 * 60;
+
+        Console.WriteLine($"The planet {planetName} is {distanceFromEarthInLightYear} lightyears away from Earth");
+        Console.WriteLine($"In meters this is {distanceInMeters:E}");
+    }
+}
