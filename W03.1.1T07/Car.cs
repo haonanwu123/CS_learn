75b41b9980f6119711c02856a405fbfaceab8ede
haonanwu123 <wuhaonan99@gmail.com>
2025-03-04 10:01:08 +0100

    done

---
+++ W03.1.1T07/Car.cs
@@ -0,0 +1,9 @@
+class Car
+{
+    public const int NumberOfWheels = 4;
+    public int Mileage;
+
+    public Car() => Mileage = 0;
+
+    public void Drive() => Mileage++;
+}
---
+++ W03.1.1T07/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace W03._1._1T07;
+
+class Program
+{
+    public static void Main()
+    {
+        Car car = new Car();
+        for (int i = 0; i < 10; i++)
+            car.Drive();
+        int mileage = car.Mileage;
+        Console.WriteLine($"The car's mileage is {mileage}");
+    }
+}
