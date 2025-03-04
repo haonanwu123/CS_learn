b74cdc8ed747414b7d07a2868216b08c147902e4
haonanwu123 <wuhaonan99@gmail.com>
2025-03-04 11:17:27 +0100

    done

---
+++ W03.1.2O01/Point.cs
@@ -0,0 +1,18 @@
+class Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static double GetEuclideanDistance(Point p1, Point p2)
+    {
+        double deltaX = p2.X - p1.X;
+        double deltaY = p2.Y - p1.Y;
+        return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+}
