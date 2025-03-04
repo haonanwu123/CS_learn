af20d95eea74166bb0d0752c31de5682554861b1
haonanwu123 <wuhaonan99@gmail.com>
2025-03-04 09:28:40 +0100

    done

---
+++ W03.1.1T03/Calculator.cs
@@ -0,0 +1,35 @@
+class Calculator
+{
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Cannot divide by zero.");
+        }
+        return a / b;
+    }
+
+    public static double Modulo(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Cannot divide by zero.");
+        }
+        return a % b;
+    }
+}
---
+++ W03.1.1T03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace W03._1._1T03;
+
+static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Give the first number:");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Give the second number:");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"{x} + {y} = {Calculator.Add(x, y)}");
+        Console.WriteLine($"{x} - {y} = {Calculator.Subtract(x, y)}");
+        Console.WriteLine($"{x} * {y} = {Calculator.Multiply(x, y)}");
+        Console.WriteLine($"{x} / {y} = {Calculator.Divide(x, y)}");
+        Console.WriteLine($"{x} % {y} = {Calculator.Modulo(x, y)}");
+    }
+}
+
