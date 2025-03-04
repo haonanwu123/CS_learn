35cfed8a70d9bcb0011cc7daddfa5ab20a24b4bc
haonanwu123 <wuhaonan99@gmail.com>
2025-03-04 09:43:28 +0100

    done

---
+++ W03.1.1T04/Circle.cs
@@ -0,0 +1,14 @@
+class Circle
+{
+    public double Radius;
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+}
