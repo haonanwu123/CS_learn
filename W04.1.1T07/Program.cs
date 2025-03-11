a0237314eda341e042bff7c035a671e0278bc4bf
haonanwu123 <wuhaonan99@gmail.com>
2025-03-11 11:37:49 +0100

    done

---
+++ W04.1.1T07/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace W04._1._1T07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person john = new Person("John Doe");
+        Console.WriteLine(john.Introduce());
+
+        Student jane = new Student("Jane Doe");
+        Console.WriteLine(jane.Introduce());
+        Console.WriteLine(jane.Status());
+        jane.Graduate();
+        Console.WriteLine(jane.Status());
+    }
+}
