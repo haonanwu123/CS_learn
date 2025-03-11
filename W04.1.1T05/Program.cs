ac1f9d55572d1b48c29232ef989fa487d6050989
haonanwu123 <wuhaonan99@gmail.com>
2025-03-11 10:31:56 +0100

    done

---
+++ W04.1.1T05/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace W04._1._1T05;
+
+class Program
+{
+    public static void Main()
+    {
+        var products = Factory.Produce("Tooth Paste", 5, 2.0);
+        double revenue = SellProducts(products);
+        Console.WriteLine($"Revenue from selling: {revenue}");
+    }
+
+    public static double SellProducts(List<Product> products)
+    {
+        double total = 0;
+        foreach (Product product in products)
+            total += product.Price;
+        return total;
+    }
+}
