07c78aa1457bcb3473e74a16ee19d4d349d2bc0d
haonanwu123 <wuhaonan99@gmail.com>
2025-03-13 11:47:39 +0100

    done

---
+++ W05.1.1T01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace W05._1._1T01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>
+        {
+            new Person("John Doe"),
+            new Student("Jane Doe")
+        };
+
+        foreach (var person in people)
+        {
+            Console.WriteLine(person.Introduce());
+
+            if (person is Student student)
+            {
+                Console.WriteLine(student.Status());
+            }
+
+        }
+    }
+}
