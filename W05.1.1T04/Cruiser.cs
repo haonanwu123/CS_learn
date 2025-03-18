c3a080e4753c51762181290b51756fd17678f893
haonanwu123 <wuhaonan99@gmail.com>
2025-03-18 10:14:25 +0100

    done

---
+++ W05.1.1T04/Cruiser.cs
@@ -0,0 +1,14 @@
+public class Cruiser : Motorcycle
+{
+    public int SeatHeight;
+
+    public Cruiser(string make, string model, int year, int seatHeight) : base(make, model, year)
+    {
+        SeatHeight = seatHeight;
+    }
+
+    public override string Ride()
+    {
+        return base.Ride() + $" with a seat height of {SeatHeight} cm";
+    }
+}
