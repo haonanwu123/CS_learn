1ad7d071542b7a6249357b991cd4f1173aa4c8ef
haonanwu123 <wuhaonan99@gmail.com>
2025-03-18 19:54:26 +0100

    done

---
+++ W05.2.1T05/Employee.cs
@@ -0,0 +1,29 @@
+public class Employee
+{
+    public Employee(string name, double salary)
+    {
+        Name = name;
+        Salary = salary;
+    }
+
+    public string Name { get; set; }
+
+    private double _salary;
+    public double Salary
+    {
+        get => _salary;
+        set
+        {
+            if (value >= 0)
+            {
+                _salary = value;
+            }
+            else
+            {
+                throw new ArgumentException("Salary must be non-negative.");
+            }
+        }
+    }
+
+    public virtual string Info => $"Name: {Name}; Salary: {Salary}";
+}
