2c104683e5dc4e1445bb357d79bfc999595c3463
haonanwu123 <wuhaonan99@gmail.com>
2025-03-18 19:12:20 +0100

    done

---
+++ W05.2.1T03/Temperature.cs
@@ -0,0 +1,22 @@
+public class Temperature
+{
+    private double _celsius;
+
+    public double Celsius
+    {
+        get { return _celsius; }
+        set
+        {
+            if (value >= -273.15)
+            {
+                _celsius = value;
+            }
+        }
+    }
+
+    public double Kelvin
+    {
+        get => _celsius + 273.15;
+        set => Celsius = value - 273.15;
+    }
+}
