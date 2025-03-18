07732783b443c1092bd7b5c5bd7b81e36ff21b2c
haonanwu123 <wuhaonan99@gmail.com>
2025-03-18 10:45:46 +0100

    done

---
+++ W05.1.1T06/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace W05._1._1T06;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Retrieving employees from a database or a file
+        List<object> employees = new()
+        {
+            new Employee(1, "Miles Dyson", "Developer"),
+            new Employee(2, "Coleman Reese", "Lawyer"),
+            new Manager (3, "Bill Lumbergh", "Manager", 5),
+            new Manager (4, "Michael Scott", "Manager", 10),
+            new Dog("Owney the mascot dog") //Not an employee
+        };
+        ListDirectReports(employees);
+    }
+
+    public static void ListDirectReports(List<object> employees)
+    {
+        foreach (var employee in employees)
+        {
+            Manager? manager = employee as Manager;
+            if (manager != null)
+            {
+                Console.WriteLine($"Manager {manager.Name} has {manager.DirectReportsCount} direct reports.");
+            }
+        }
+    }
+}
