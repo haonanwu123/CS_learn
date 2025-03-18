c1e4a15151f58d8ed84bfac992739be18833c874
haonanwu123 <wuhaonan99@gmail.com>
2025-03-18 11:25:55 +0100

    done

---
+++ W05.1.1T07/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace W05._1._1T07;
+
+public class Program
+{
+    public static void Main()
+    {
+        SevenElevenVariant game = new SevenElevenVariant(0);
+
+        game.PlayGame();
+    }
+}
