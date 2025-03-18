fd57d4fd06037fe8b2a1c4914887f5b391943c02
haonanwu123 <wuhaonan99@gmail.com>
2025-03-18 11:41:59 +0100

    done

---
+++ W05.1.1T08/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace W05._1._1T08;
+
+public class Program
+{
+    public static void Main()
+    {
+        Shop shop = new(new List<string>() { "Bread", "Egg" });
+        List<string> shoppingBag = new();
+
+        shoppingBag.Add(shop.Buy("Bread"));
+
+        if (shop.HaveInInventory("Egg"))
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                shoppingBag.Add(shop.Buy("Egg"));
+            }
+        }
+
+        //Wife checks shopping bag content.
+        //Do not modify these lines!
+        Console.WriteLine(shoppingBag.Count(s => s == "Bread")); //Expected 1
+        Console.WriteLine(shoppingBag.Count(s => s == "Egg")); //Expected 12
+    }
+}
