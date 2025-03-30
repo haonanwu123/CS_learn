44af2a1d1f781e08188698d6941d337df41398ba
haonanwu123 <wuhaonan99@gmail.com>
2025-03-30 18:03:46 +0200

    done

---
+++ W06.2.1T05/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace W06._2._1T05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee john = new Employee("John", "Doe", "john.doe@example.com");
+        Employee jane = new Manager("Jane", "Doe", "jane.doe@example.com", "Sales");
+        Employee bob = new SalesPerson("Bob", "Smith", "bob.smith@example.com", 100000);
+
+        john.PrintEmployeeInfo();
+        jane.PrintEmployeeInfo();
+        bob.PrintEmployeeInfo();
+
+        john.PrintEmployeeInfo();
+        ((Manager)jane).PrintEmployeeInfo();
+        ((SalesPerson)bob).PrintEmployeeInfo();
+    }
+}
