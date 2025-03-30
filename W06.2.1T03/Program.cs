2d085c458c532c372f91d63009c1087e5346997c
haonanwu123 <wuhaonan99@gmail.com>
2025-03-30 17:53:00 +0200

    done

---
+++ W06.2.1T03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace W06._2._1T03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Bill> bills = new List<Bill>
+        {
+            new ElectricityBill(50, "John Smith"),
+                new ElectricityBill(75, "Jane Doe"),
+                new GasBill(100, "Bob Johnson", false),
+                new GasBill(125, "John Doe", true)
+        };
+
+        foreach (var bill in bills)
+        {
+            Console.WriteLine(bill.GetDescription());
+        }
+
+        double total = bills.Sum(bill => bill.Amount);
+        Console.WriteLine($"Total amount: {(int)total}");
+    }
+}
